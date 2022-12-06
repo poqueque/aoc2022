2f087bc9cbcac0fef94a724ff00d25cc540f135a
Edi <poqueque@yahoo.es>
2022-12-06 23:13:59 +0100

    Day 06

--- lib/days/day06.dart
+++ lib/days/day06.dart
@@ -4,11 +4,21 @@ class Day06 extends Day {
 
   @override
   part1() {
-    return 0;
+    for (int i=0; i<inputString.length-3; i++){
+      var r = inputString.substring(i,i+4);
+      if (r.runes.toSet().toList().length == 4)
+        return i+4;
+    }
+    return inputString;
   }
 
   @override
   part2() {
-    return "0";
+    for (int i=0; i<inputString.length-13; i++){
+      var r = inputString.substring(i,i+14);
+      if (r.runes.toSet().toList().length == 14)
+        return i+14;
+    }
+    return inputString;
   }
 }
